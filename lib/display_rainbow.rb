8ac909d65c953576ce920fc5c9517ee16f333d45
Phuong Vu <phuong.pmv@gmail.com>
2017-10-06 17:39:59 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  print("#{colors[0][0].upcase}: #{colors[0]}")
+  for i in 2..colors.length
+    print(", #{colors[i][0].upcase}: #{colors[i]}")
+  end
+end
+    
+    
